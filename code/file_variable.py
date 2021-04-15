91ff2139203e282a70ce68c56ce1fab6a558e993
johnybill <r@lefevere.com>
2021-04-15 23:46:25 +0200

    :hammer: :alien: Refacto variable in file_variable

--- code/file_variable.py
+++ code/file_variable.py
@@ -8,26 +8,31 @@ import shutil
 # ===========================================
 # CHEMIN DES DOSSIERS
 # ===========================================
-PATH_DATA = "../data"
-PATH_DATA_IMAGE = PATH_DATA + "/image"
-PATH_DATA_VECTEUR = PATH_DATA + "/vecteur"
+l = os.path.abspath(".")
 
-PATH_DATA_CNN = PATH_DATA + "/CNN"
-PATH_DATA_CNN_TRAIN = PATH_DATA_CNN + "/TRAIN"
-PATH_DATA_CNN_TEST = PATH_DATA_CNN + "/TEST"
+PATH_ORIGINE = l[:l.find("Open-Food-Fact") + len("Open-Food-Fact")]
+PATH_ORIGINE += '/'
 
-PATH_DATA_IMAGE_SCRAP = PATH_DATA + "/Data_a_scrap"
+PATH_DATA = PATH_ORIGINE + "data/"
+PATH_DATA_IMAGE = PATH_DATA + "picture/"
+PATH_DATA_VECTOR = PATH_DATA + "vector/"
 
-PATH_DATA_KNN = PATH_DATA + "/KNN"
+PATH_DATA_CNN = PATH_DATA + "CNN/"
+PATH_DATA_CNN_TRAIN = PATH_DATA_CNN + "TRAIN/"
+PATH_DATA_CNN_TEST = PATH_DATA_CNN + "TEST/"
+
+PATH_DATA_PICTURE_SCRAP = PATH_DATA + "Data_a_scrap/"
+
+PATH_DATA_KNN = PATH_DATA + "KNN/"
 
 # ===========================================
 # CHEMIN DES FICHIERS
 # ===========================================
 
-PATH_DATA_CNN_VG16 = PATH_DATA_CNN + "/vg16.txt"
-PATH_DATA_VECTEUR_FILE = PATH_DATA_VECTEUR + "/vector.csv"
-PATH_DATA_TRAIN = PATH_DATA_IMAGE_SCRAP + "/data_url_image_train_110k_v2.csv"
-PATH_DATA_TEST = PATH_DATA_IMAGE_SCRAP + "/data_url_image_test_110k_v2.csv"
+PATH_DATA_CNN_VG16 = PATH_DATA_CNN + "vg16.txt"
+PATH_DATA_VECTOR_FILE = PATH_DATA_VECTOR + "vector.csv"
+PATH_DATA_TRAIN = PATH_DATA_PICTURE_SCRAP + "data_url_image_train_110k_v2.csv"
+PATH_DATA_TEST = PATH_DATA_PICTURE_SCRAP + "data_url_image_test_110k_v2.csv"
 
 
 # ===========================================
@@ -47,12 +52,14 @@ def implement(path_image):
         os.mkdir(PATH_DATA)
     if not os.path.exists(PATH_DATA_IMAGE):
         os.mkdir(PATH_DATA_IMAGE)
-    if not os.path.exists(PATH_DATA_VECTEUR):
-        os.mkdir(PATH_DATA_VECTEUR)
+    if not os.path.exists(PATH_DATA_VECTOR):
+        os.mkdir(PATH_DATA_VECTOR)
     if not os.path.exists(PATH_DATA_CNN):
         os.mkdir(PATH_DATA_CNN)
     if not os.path.exists(PATH_DATA_KNN):
         os.mkdir(PATH_DATA_KNN)
+    if not os.path.exists(PATH_DATA_CNN_TEST):
+        os.mkdir(PATH_DATA_CNN_TEST)
     if path_image == '':
         return None
     elif os.path.exists(path_image):
