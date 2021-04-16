0f4a0cb83469dd4057b89587c3b72ad9bac19779
johnybill <r@lefevere.com>
2021-04-16 16:55:03 +0200

    :books: :alien: main.py docstring french to english

--- code/main.py
+++ code/main.py
@@ -31,30 +31,30 @@ DF_PRODUIT_TEST = DF_PRODUIT_TEST.set_index('code')
 
 
 def picture_to_pictures(path_picture, nb_pictures):
-    """retourne la liste des chemins des nb_pictures similaire à l'image
-    stocker dans le path image
+    """return the list of paths of nb_pictures similar to the image stored
+    in the image path
 
     Args:
-        path_picture (str): chemin du fichier image à tester
-        nb_pictures (int): nombre d'image simmilaire qu'on veut retourner
+        - path_picture (str): path of the image file to test
+        - nb_pictures (int): number of similar images we want to return
 
     Returns:
-        list(str): liste des chemins des fichiers images
+        list(str): list of image file paths
     """
     list_id = picture_to_list_code(path_picture, nb_pictures)
     return [MODEL_CNN.find_image(code) for code in list_id]
 
 
 def picture_to_list_code(path_picture, nb_pictures):
-    """return les id nb_pictures similaires à l'image stocker
-    dans le chemins path_picture
+    """return the ids nb_pictures similar to the image
+    stored in the path_picture
 
     Args:
-        path_picture (str): chemin du fichier image à tester
-        nb_pictures (int): nombre d'id image qu'on veut retourner
+        - path_picture (str): path of the image file to test
+        - nb_pictures (int): number of image ids we want to return
 
     Returns:
-        list(str): liste des id images similaires
+        list(str): list of id similar images
     """
     vec = MODEL_CNN.image_to_vector(path_picture)
     return MODEL_KNN.find_similar_vector_id(vec, nb_pictures)
@@ -67,12 +67,11 @@ def find_path_sim_picture_to_code(code):
 
 
 def show_image(path_picture, nb_pictures):
-    """affiche l'image dans le chemin path et les nb_pictures similaires
-    à cette image
+    """displays the image in the path and the nb_pictures similar to this image
 
     Args:
-        path_picture (str): chemin du fichier image à afficher
-        nb_pictures (int): nombre d'image similaire que l'on veut afficher
+        - path_picture (str): path of the image file to display
+        - nb_pictures (int): number of similar images to be displayed
     """
     print("=============================")
     vector = MODEL_CNN.changer_format(path_picture, (224, 224))[0]
@@ -94,32 +93,32 @@ def show_image(path_picture, nb_pictures):
 
 
 def display_data_vector_available():
-    """retourne les noms des datasets créer par les différent modèle
+    """returns the names of the datasets created by the different models
 
     Returns:
-        [string]: liste des différents noms
+        [string]: list of different names
     """
     return os.listdir(fv.PATH_DATA_VECTOR)
 
 
 def all_implement(path_picture):
-    """implement les différent dossier nécessaire au déploiment du projet,
-    déplacer les images du path_picture vers le dossiers data/image pour
-    pouvoir les tester. instancie les class MODEL_CNN et MODEL_KNN
+    """implement the different folders necessary to deploy the project,
+    move the images from the path_picture to the data/image folder to
+    be able to test them. instantiate the MODEL_CNN and MODEL_KNN classes
 
-    - l'instance MODEL_CNN permet de choisir le modele pour transformer
-    l'image en vecteur ainsi que les différents fonction d'entrainement
-    et de teste de performances.
+    - the MODEL_CNN instance allows to choose the model to transform the
+    image into a vector as well as the different training and performance
+    test functions.
 
-    - l'instance MODEL_KNN permet de choisir le modele pour choisir
-    les images similaire
+    - the instance MODEL_KNN allows to choose the model to select the similar
+    images.
 
-    si vous avez déjà mis les images dans le dossier data/image/
-    mettait seulement  '' pour le path_picture
+    if you have already put the images in the folder data/image/ put only
+    '' for the path_picture
 
     Args:
-        path_picture (string): chemin absolue du dossier contenant
-        les images à déplacer dans le dossier data/image
+        path_picture (string): absolute path of the folder containing the
+        images to be moved to the data/image folder
     """
     fv.implement(path_picture)
     global MODEL_CNN, MODEL_KNN
@@ -131,12 +130,12 @@ def all_implement(path_picture):
 
 
 def choice_vector_database(name_database):
-    """choisi le dataset vecteur utilisé par le model_KNN
-    pour la recherche des images similaires
+    """Choose the vector dataset used by the model_KNN
+    to search for similar images
 
     Args:
-        name_database (string): nom du datasets à utilisé,
-        la liste des noms peut se trouver avec la fonction
+        - name_database (string): name of the dataset to be used,
+        the list of names can be found with the function
         display_data_vector_available
     """
     path = fv.PATH_DATA_VECTOR + name_database
@@ -144,17 +143,16 @@ def choice_vector_database(name_database):
 
 
 def set_up_model(type_model, name_model):
-    """charger le modele name_model dans le type_model qui est soit
-    KNN ou CNN
+    """Load the model name_model into the type_model which is either KNN or CNN
 
     Args:
-        type_model (string): valeur "KNN" ou "CNN"
-        name_model (string): name file model
+        - type_model (string): value "KNN" ou "CNN"
+        - name_model (string): name file model
 
     exemple:
-        # changer model VGG16 par Mobilenet
+        # change model VGG16 par Mobilenet
         >>> set_up_model("CNN", "mobile_net")
-        # revenir sur le modele VGG16
+        # remove on the modele VGG16
         >>> set_up_model("CNN", "VGG16")
     """
     if type_model == "KNN":
@@ -178,27 +176,26 @@ def train_knn(name_database, verbose=False):
 
 
 def train_cnn(nb_pictures=0, format=(224, 224), verbose=False):
-    """entraine le modele stocké dans la variable CNN_MODEL avec nb_pictures,
-    avec les images préprocessées avec la variable format pour définir
-    le format de l'image
+    """Trains the model stored in the variable CNN_MODEL with nb_pictures,
+    with the images preprocessed with the variable format to define the
+    format of the image
 
     Args:
-        nb_pictures (int, optional): nombre d'image pour l'entrainement du model
-         si nb_pictures = 0 toutes les images stockées dans data/image sont
-         utilisées
-         . Defaults to 0.
-        format ((int, int)), optional): format de l'image après le
-        préprocessing. Defaults to (224, 224).
-        verbose (bool, optional): affiche les différents étapes réaliées
-        et le temps de calcul. Defaults to False.
+        - nb_pictures (int, optional): number of images for training the model
+        if nb_pictures = 0 all images stored in data/image are used.
+        Defaults to 0.
+        - format ((int, int)), optional): image format after preprocessing.
+        Defaults to (224, 224).
+        - verbose (bool, optional): displays the different steps performed
+        and the calculation time. Defaults to False.
 
     Exemple:
-        # entrainer le modele sur toutes les images
+        # train the model on all images
         >>> train_cnn()
-        # entrainer le modele sur 2000 images, au format (128, 128)
+        # train the model on 2000 images, format (128, 128)
         >>> train_cnn(nb_pictures=2000, format=(128, 128))
-        # entrainer le modele sur toutes les images et affiché
-        # le temps de calcul
+        # train the model on all images and display
+        # calculation time
         >>> train_cnn(verbose=True)
     """
     if verbose:
@@ -240,13 +237,15 @@ def create_dataframe_vector(list_name_picture):
 
 
 def create_database_vectorize(name_database="vector.csv", verbose=False):
-    """contruit un dataset vecteur de l'ensemble des images stocker
-    dans le dossier data/image avec le modele CNN utilisé dans
-    la variable MODEL_CNN et le stocker dans le dossier data/vecteur
+    """constructs a vector dataset of all the images stored in the data/image
+    folder with the CNN model used in the MODEL_CNN variable and stores it in
+    the data/vector folder
 
     Args:
-        verbose (bool, optional): [afficher les
-        différents étapes réalisé et le temps de calcul]. Defaults to False.
+        - name_database=(bool, optional): [database name].
+         Defaults to "vector.csv"
+        - verbose (bool, optional): [Display the different steps performed
+        and the calculation time]. Defaults to False.
     """
     list_pictures = os.listdir(fv.PATH_DATA_IMAGE)
     if verbose:
@@ -261,14 +260,14 @@ def create_database_vectorize(name_database="vector.csv", verbose=False):
         index = new_index
         if verbose:
             tps2 = time.time()
-            print(f"nombre traité: {new_index}")
+            print(f"number processed: {new_index}")
             print(f"Execution time: {tps2 - tps1}")
     reduce_list_image = list_pictures[index:]
     df = pd.concat([df, create_dataframe_vector(reduce_list_image)])
     index = new_index
     if verbose:
         tps2 = time.time()
-        print(f"nombre traité: {len(list_pictures)}")
+        print(f"number processed: {len(list_pictures)}")
         print(f"Execution time: {tps2 - tps1}")
     df = df.iloc[1:]
     if name_database == "vector.csv":
@@ -285,10 +284,13 @@ def test_performance_cnn(nb_picturess_test=5, verbose=False):
     """[summary]
 
     Args:
-        nb_picturess_test (int, optional): [description]. Defaults to 5.
+        - nb_picturess_test (int, optional): [description]. Defaults to 5.
+        - verbose (bool, optional): [afficher les différents étapes
+        réalisé et le temps de calcul]. Defaults to False.
+
     """
     if verbose:
-        nb_pictures_fait = 0
+        nb_pictures_done = 0
         print("=========================")
         print("Start create new dataset")
         tps1 = time.time()
@@ -299,11 +301,11 @@ def test_performance_cnn(nb_picturess_test=5, verbose=False):
     table_index[-1] = 0
     for path_picture, code_origin_image in zip(list_pictures, images_a_tester):
         if verbose:
-            if (nb_pictures_fait % 300) == 0:
+            if (nb_pictures_done % 300) == 0:
                 tps2 = time.time()
-                print(f"nombre traité: {nb_pictures_fait}")
+                print(f"number processed: {nb_pictures_done}")
                 print(f"Execution time: {tps2 - tps1}")
-            nb_pictures_fait += 1
+            nb_pictures_done += 1
         try:
             liste_id = picture_to_list_code(path_picture, nb_picturess_test)
         except:
